eafdbfd5a44dd7f8955e1521d8a0be878ff202c2
manishmahuva1-dev <manishmahuva1@gmail.com>
2026-07-06 03:02:08 +0530

    Update settings.py

--- familytree_project/settings.py
+++ familytree_project/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = config('SECRET_KEY', default='your_secret_key_here')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
